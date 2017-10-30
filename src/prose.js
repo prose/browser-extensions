b8282afbaed6268eb3fdaf22a6bdd4aefbe298e2
Narno <arnaud@ligny.org>
2017-10-30 16:31:39 +0100

    Code linting.

--- src/prose.js
+++ src/prose.js
@@ -3,30 +3,36 @@
   document.addEventListener('DOMSubtreeModified', redraw);
 
   function redraw() {
-    var parts = location.pathname.substring(1).split('/');
-    var user = parts[0];
-    var repo = parts[1];
-    var branch = parts[3];
-    var path = parts.slice(4).join('/');
+    const parts = location.pathname.substring(1).split('/');
+    const user = parts[0];
+    const repo = parts[1];
+    const branch = parts[3];
+    const path = parts.slice(4).join('/');
 
-    if (!user || !/^\w[\w-]+$/.test(user)) return;
+    if (!user || !/^\w[\w-]+$/.test(user)) {
+      return;
+    }
 
     // Sha Test
-    if (/^[0-9a-f]{40}$/.test(branch)) return;
+    if (/^[0-9a-f]{40}$/.test(branch)) {
+      return;
+    }
 
-    var root = document.querySelector('.file-actions');
-    if (!root) return;
+    const root = document.querySelector('.file-actions');
+    if (!root) {
+      return;
+    }
 
-    var group = document.querySelector('.file-actions > .BtnGroup');
+    const group = document.querySelector('.file-actions > .BtnGroup');
     if (!group) {
-      group = document.createElement('div');
-      div.className = 'BtnGroup';
+      const group = document.createElement('div');
+      group.className = 'BtnGroup';
       root.appendChild(group);
     }
 
-    var a = document.querySelector('#prose');
+    let a = document.querySelector('#prose');
     if (!a) {
-      var link = document.createElement('a');
+      const link = document.createElement('a');
       link.id = 'prose';
       link.innerHTML = '<span class="prose-icon"></span>Edit in Prose';
       link.className = 'btn btn-sm BtnGroup-item';
